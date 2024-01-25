c607d4f406c551956716ed0d75340d91986985f0
Albert Boehmler <ajboehmler@gmail.com>
2024-01-25 07:48:44 -0500

    Fix send app message component

--- qroma-lib/qroma-io/QromaIoSendAppMessageComponent.tsx
+++ qroma-lib/qroma-io/QromaIoSendAppMessageComponent.tsx
@@ -17,10 +17,14 @@ export const QromaIoSendAppMessageComponent = <T extends object, U extends objec
   props: IQromaIoSendAppMessageComponentProps<T, U>
 ) => {
   
+  const [qromaAppResponse, setQromaAppResponse] = useState(props.responseMessageType.create());
+  const [isPortConnected, setIsPortConnected] = useState(false);
+  
   const inputs: IUseQromaAppWebSerialInputs<T, U> = {
     onQromaAppResponse: (appMessage: U) => {
-      console.log("QromaRequestForm - onQromaAppResponse!!");
+      console.log("QromaIoSendAppMessageComponent - onQromaAppResponse!!");
       console.log(appMessage);
+      setQromaAppResponse(appMessage);
     },
     onQromaCommResponse: (message: QromaCommResponse) => {
       console.log("QromaRequestForm - onQromaCommResponse!!");
@@ -28,7 +32,13 @@ export const QromaIoSendAppMessageComponent = <T extends object, U extends objec
     },
     commandMessageType: props.requestMessageType,
     responseMessageType: props.responseMessageType,
-    onPortRequestResult: (requestResult: PortRequestResult) => { }
+    onPortRequestResult: (requestResult: PortRequestResult) => {
+      if (requestResult.success) {
+        setIsPortConnected(true);
+      } else {
+        setIsPortConnected(false);
+      }
+    }
   }
   const qromaAppWebSerial = useQromaAppWebSerial(inputs);
 
@@ -89,6 +99,9 @@ export const QromaIoSendAppMessageComponent = <T extends object, U extends objec
         <button onClick={() => sendRequest() }>Send App Message</button> :
         <button onClick={() => startConnection() }>Start Connection!</button> 
       }
+      <div>
+        App Response: {JSON.stringify(qromaAppResponse)}
+      </div>
     </div>
   )
 }
